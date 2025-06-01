9690b39eb2c9afa0824e3cc1d61ba9c2e220c10f
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-01 21:50:37 +0530

    maximum sum subarry

---
+++ maximun_sum_kadane_algo.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n= nums.size();
+        long long sum = 0;
+        long long maxi = INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+            if(sum>maxi)
+            {
+                maxi=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+};
