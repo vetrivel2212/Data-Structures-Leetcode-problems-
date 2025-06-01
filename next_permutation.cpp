3d0f4fc49b46942941a94fc3f3c8c8d7a356fca1
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-01 16:56:24 +0530

    Add files via upload

---
+++ next_permutation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+
+        // std::next_permutation(nums.begin(), nums.end());  // Use std:: to avoid name clash
+        // for (auto i : nums) {
+        //     cout << i << " ";
+        // }
+        // cout << endl;
+        int n = nums.size();
+        int ind = -1;
+
+        // Step 1: Find the breakpoint
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        // Step 2: If no breakpoint, reverse the array
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+        }
+        else {
+            // Step 3: Find the next greater element from the right
+            for (int i = n - 1; i > ind; i--) {
+                if (nums[i] > nums[ind]) {
+                    swap(nums[i], nums[ind]);
+                    break;  // ✅ break after the first valid swap
+                }
+            }
+
+            // Step 4: Reverse the tail
+            reverse(nums.begin() + ind + 1, nums.end());
+        }
+
+        // Optional: Print for verification
+        for (auto i : nums) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+};
