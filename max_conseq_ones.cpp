0724c5ec30ca72ec3bd3948c321cef7537531f92
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-12 20:57:26 +0530

    Create max_conseq_ones.cpp

---
+++ max_conseq_ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxi = 0,count = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1) count++;
+            else count = 0;
+            maxi = max(count,maxi);
+        }
+        return maxi;
+    }
+};
