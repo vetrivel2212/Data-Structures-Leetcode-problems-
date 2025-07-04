978bdeea3d73759cd82eaf5c8eda19c7fa54f992
vetrivel2212 <vetrivel221205@gmail.com>
2025-07-04 20:57:34 +0530

    Create Container_with_most_water.cpp

---
+++ Container_with_most_water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int max_area = 0;
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            max_area = max(max_area, h * w);
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return max_area;
+    }
+};
